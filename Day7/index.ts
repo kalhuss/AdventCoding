e52ce0a83388b16778641f0f901d154d119e51b0
kkh10 <kkh10@aber.ac.uk>
2022-12-08 04:21:27 +0000

    Day 7 Complete

--- Day7/index.ts
+++ Day7/index.ts
@@ -2,56 +2,60 @@ import { readFileSync } from "fs";
 
 const file = readFileSync("input.txt", "utf8");
 
-// Read the file line by line and return each line as an array
-function readLines(file: string) {
-    const lines = file.split("\n");
-    const linesSplit = lines.map((line) => line.split(" "));
-    return linesSplit;
-}
+function dirSize(file: string) {
+    let lines = file.split("\n");
+    let path: string[] = [];
+    let sizeDict: { [key: string]: number } = {};
 
-function checkLine(line : string[][]){
-    let directory : string[] = ["/"];
-    let fileSize : number[] = [];
-    let stored : number[] = [];
-    //Loop through each line
-    for(let i = 0; i < line.length; i++){
-        if(directory.length > 1){
-            //If it's a command
-            if(line[i][0] == "$"){
-                //If the command is cd
-                if(line[i][1] == "cd"){
-                    //If the command is cd ..
-                    if(line[i][2] == ".."){
-                        //Remove the last element of the directory array
-                        directory.pop();
-                    //If the command is cd /
-                    } else if(line[i][1] == "/"){
-                        //Reset the directory array
-                        directory = ["/"];
-                    //If the command is cd <directory>
-                    } else{
-                        //Add the directory to the directory array
-                        directory.push(line[i][2]);
-                    }
-                }
-            } else{
-                //add the file size to the fileSize array until the directory has been popped back to the root, when its back at the root it needs to move all the filesizes to the stored array
-                if(line[i][0].match(/\d+/)){
-                    fileSize.push(parseInt(line[i][0]));
-                }
+    for (let line of lines) {
+        line = line.replace(/(\r\n|\n|\r)/gm, "");
+        if (line.startsWith("$ cd")) {
+            if (line.startsWith("$ cd /")) {
+                path = [];
+            } else if (line.endsWith("..")) {
+                path.pop();
+            } else {
+                path.push(line.substring(5));
             }
+        } else if (line.startsWith("$ ls")) {
+            continue;
+        } else if (line.startsWith("dir")) {
+            continue;
         } else {
-            //If the directory is at the root push the file size to the stored array and empty the fileSize array
-            for(let j = 0; j < fileSize.length; j++){
-                stored.push(fileSize[j]);
+            let size = parseInt(line.substring(0, line.indexOf(" ")));
+            for (let i = 0; i <= path.length; i++) {
+                let key = path.slice(0, i).join("/");
+                sizeDict[key] = (sizeDict[key] || 0) + size;
             }
-            fileSize = [];
         }
-
     }
-    console.log(directory);
-    console.log(fileSize);
-    console.log(stored);
+    return sizeDict;
 }
 
-checkLine(readLines(file));
+function part1(file: string) {
+    const sizeDict = dirSize(file);
+    let sum = 0;
+    for (let value of Object.values(sizeDict)) {
+        if (value < 100000) {
+            sum += value;
+        }
+    }
+    console.log(sum);
+}
+
+function part2(file: string) {
+    const totalSize = dirSize(file)[""];
+    const max = 70000000 - 30000000;
+    const spaceNeeded = totalSize - max;
+    let minVal = Number.MAX_SAFE_INTEGER;
+
+    for (let value of Object.values(dirSize(file))) {
+        if (value >= spaceNeeded) {
+            minVal = Math.min(minVal, value);
+        }
+    }
+    console.log(minVal);
+}
+
+part1(file);
+part2(file);
